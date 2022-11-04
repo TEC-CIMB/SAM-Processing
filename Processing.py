55be64ea631c018b81f9541d740fd633578d70d1
aMorocho <josem.amorocho@urosario.edu.co>
2022-11-03 22:54:14 -0600

    Actualizacion de datos

--- Processing.py
+++ Processing.py
@@ -142,14 +142,14 @@ def Syncronize(data):
         corr_max = argmax(corr, axis=0)
         lag = append(lag, median([lags[corr_max[0]], lags[corr_max[1]],
                                   lags[corr_max[2]]]))
-#    n = append(ref, n)
-#    if 0 > min(lag):
-#        lag += abs(min(lag))
-#    for s in range(0, len(n)):
-#        data[n[s]] = data[n[s]].iloc[int(lag[s]):, :]
-#        time = data[n[s]]['TimeStamp']
-#        time = time-time.iloc[0]
-#        data[n[s]]['TimeStamp'] = time
+    n = append(ref, n)
+    if 0 > min(lag):
+        lag += abs(min(lag))
+    for s in range(0, len(n)):
+        data[n[s]] = data[n[s]].iloc[int(lag[s]):, :]
+        time = data[n[s]]['TimeStamp']
+        time = time-time.iloc[0]
+        data[n[s]]['TimeStamp'] = time
     return data
 # _______________________________________________________________________________
 
@@ -169,12 +169,76 @@ def Same_size(data):
 # _______________________________________________________________________________
 
 
-def timeMove(data,seg):
+def timeMove(data, seg):
     """
     This function adds seg (in seconds) to TimeStamp of data
     """
-    
+
     time = data['TimeStamp']
     new_time = time + seg
     data['TimeStamp'] = new_time
     return data
+# _______________________________________________________________________________
+
+
+def Plot_Ang(test, time, ang_ankle, ang_knee, ang_hip):
+    """
+    This function plots the angles of the joints.
+    """
+    fig, ((fe1, aa1, ro1), (fe2, aa2, ro2), (fe3, aa3, ro3)
+          ) = subplots(3, 3, figsize=(9.6, 5.5))
+    fe1.plot(time, ang_ankle['Fle_Ext'])
+    aa1.plot(time, ang_ankle['Abd_Abb'])
+    ro1.plot(time, ang_ankle['Rot'])
+
+    fe2.plot(time, ang_knee['Fle_Ext'])
+    aa2.plot(time, ang_knee['Abd_Abb'])
+    ro2.plot(time, ang_knee['Rot'])
+
+    fe3.plot(time, ang_hip['Fle_Ext'])
+    aa3.plot(time, ang_hip['Abd_Abb'])
+    ro3.plot(time, ang_hip['Rot'])
+
+    fig.suptitle('Angle '+test)
+    fe1.set_xlabel('Time (s)')
+    fe1.set_ylabel('Angles(°)')
+    fe1.set_title('Ankle Flex-Ext')
+    aa1.set_xlabel('Time (s)')
+    aa1.set_ylabel('Angles(°)')
+    aa1.set_title('Ankle Abd-Abb')
+    ro1.set_xlabel('Time (s)')
+    ro1.set_ylabel('Angles(°)')
+    ro1.set_title('Ankle Rotation')
+    fe1.margins(0, 0.1)
+    aa1.margins(0, 0.1)
+    ro1.margins(0, 0.1)
+
+    fe2.set_xlabel('Time (s)')
+    fe2.set_ylabel('Angles(°)')
+    fe2.set_title('Knee Flex-Ext')
+    aa2.set_xlabel('Time (s)')
+    aa2.set_ylabel('Angles(°)')
+    aa2.set_title('Knee Abd-Abb')
+    ro2.set_xlabel('Time (s)')
+    ro2.set_ylabel('Angles(°)')
+    ro2.set_title('Knee Rotation')
+    fe2.margins(0, 0.1)
+    aa2.margins(0, 0.1)
+    ro2.margins(0, 0.1)
+
+    fe3.set_xlabel('Time (s)')
+    fe3.set_ylabel('Angles(°)')
+    fe3.set_title('Hip Flex-Ext')
+    aa3.set_xlabel('Time (s)')
+    aa3.set_ylabel('Angles(°)')
+    aa3.set_title('Hip Abd-Abb')
+    ro3.set_xlabel('Time (s)')
+    ro3.set_ylabel('Angles(°)')
+    ro3.set_title('Hip Rotation')
+    fe3.margins(0, 0.1)
+    aa3.margins(0, 0.1)
+    ro3.margins(0, 0.1)
+
+    fig.tight_layout()
+    show()
+    return None
